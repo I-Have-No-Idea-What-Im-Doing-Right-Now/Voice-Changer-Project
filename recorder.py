4afec21ee7d3c32d3c50e78cbfd15bf093346b8e
Soren <Sorenlee360@gmail.com>
2024-11-01 15:09:29 -0500

    changes

--- recorder.py
+++ recorder.py
@@ -1,15 +1,16 @@
 import sounddevice as sd
 import numpy as np
-
+from scipy.io.wavfile import write
+print(sd.query_devices())
 def recAndPlayback():
 
     fs = 44100 # Sampling frequency
     duration = 2  # seconds
 
-    myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=1)
+    myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=2)
     sd.wait()
     sd.stop()
-    print(myrecording)
-    sd.play(myrecording, samplerate=fs, blocking=True)
-    sd.stop()
+    write('test.wav', fs, myrecording)
+    #sd.play(myrecording, samplerate=fs, blocking=True)
+    #sd.stop()
            
